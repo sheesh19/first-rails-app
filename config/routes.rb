b64a341047eb7075737db8c998264d9b8e8f041c
Sheila Leveille <sbleveille@gmail.com>
2020-05-25 10:34:53 +1000

    added controllers, routes, and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # VERB PREFIX, CONTROLLER#ACTION
+
+  #get "home", to: "pages#home"
+
+  root to: "pages#home"
+
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact", as: :contact_us
+
+  get "flats", to: "flats#index"
 end
